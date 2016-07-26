2d6d791a07aaf1cf297def38524cebac4d25a10a
ronchi <ron.chi@vidaxl.com>
2016-07-26 14:06:56 +0800

    add daemon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 var core = require('./lib/messagebus');
+var daemon = require('./lib/daemon');
 var messageBus = new core("amqp://localhost");
 var keys = process.argv.slice(2);
 console.log(keys);
@@ -10,6 +11,9 @@ if (keys.length < 1) {
 }
 
 var action = keys[0];
+
+
+
 
 if(action == "send"){
 	var exchange = keys[1];
@@ -51,6 +55,32 @@ if(action == "send"){
 	var rk = keys[3];
 
 	messageBus.unbindRoutingKey(queue, exchange, rk);
+} else if(action == 'daemon-start'){
+	var name = keys[1];
+	daemon.start(name, 'index.js', ['listen', 'test-exchange', 'test-queue-2', 'test-rk'],null, function(process){
+		console.log(process);
+	});
+
+} else if(action == 'daemon-stop') {
+	var name = keys[1];
+	daemon.stop(name, function (apps) {
+		
+	});
+} else if(action == 'daemon-delete'){
+	var name = keys[1];
+	daemon.delete(name, function (apps) {
+
+	});
+} else if(action == 'daemon-restart'){
+	var name = keys[1];
+	daemon.restart(name, function (apps) {
+
+	});
+} else if(action == 'daemon-desc'){
+	var name = keys[1];
+	daemon.desc(name, function (apps) {
+		console.log(apps);
+	});
 } else {
 	console.log("wrong action");
 }
