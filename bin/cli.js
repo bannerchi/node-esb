737b85744310014ef296c0c87df50fc4e6bb13ab
ronchi <ron.chi@vidaxl.com>
2016-08-04 15:15:25 +0800

    add todo

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var program = require('commander');
-
+//TODO 加一个结构生成器 build 功能
 program
 	.version('0.0.1')
 	.command('listen [id]', 'start a listener with listener Id')
--- core/dispatcher.js
+++ core/dispatcher.js
@@ -1,3 +1,15 @@
 /**
- * dispatcher for listener
+ TODO 调度器
+ dispatcher for listener
+ listenerId = 1  listener
+ 1. mysql
+ 2. f1 = base path + exchange name  ex: ../connectors/myExchange
+ 3. var allQueue = require('require-dir')(f1); //判断dir 是否存在, 是否有文件
+ var listener = allQueue[listener['name']];
+ exports.startListener = function(){};
+ 基础 ： ./connectors
+ ../myExchange
+ .../myQueue1.js
+ .../myQueue2.js
+ .../myQueue3.js
  */
